b8ba0a3ba50a7ac8d2d94ae666506cfa7572ef87
Larkoss <klarko01@ucy.ac.cy>
2020-11-24 23:05:36 +0200

    Added documentation

--- byteManipulation.c
+++ byteManipulation.c
@@ -1,6 +1,15 @@
+//Purpose of this function is:
+//1. Given an array of characters(the text of the file), transform it to array of bits
+//2. Insert the bits into a 2d array that represents the pixel of the image
+//3. Change the position of each bit to match the order explained in Question
+//4. Put the bits back, in a 1d array
+//5. Traverse the 1d array(in ) and set each pixel to 
+
 #include "partC.h"
 #include <math.h>
 
+
+//Function to print array
 void printArr(int **arr, int h, int w)
 {
 	for (int i = 0; i < h; i++)
@@ -12,6 +21,7 @@ void printArr(int **arr, int h, int w)
 	printf("\n");
 }
 
+//Swap position of two elements in array
 void swap(int *a, int *b)
 {
 	int temp = *a;
@@ -19,6 +29,7 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+//Swap each element of a row of the array
 void FlipColumn(int **array, int column, int rows)
 {
 	// Flip column 'column' of an array that has n rows.
@@ -28,6 +39,7 @@ void FlipColumn(int **array, int column, int rows)
 	}
 }
 
+//Mirror the array vertically.Swap first rows with last rows
 void VFlipArray(int **array, int columns, int rows)
 {
 	for (int column = 0; column < columns; column++)
@@ -36,6 +48,8 @@ void VFlipArray(int **array, int columns, int rows)
 	}
 }
 
+//Read a char array the represents the text of a file
+//Turn each byte into bits. Fill the remaining with zeros.
 int *createBitImage(int height, int width, char *image, int imageSize)
 {
 	int *bitImage = (int *)malloc(sizeof(int) * width * height * 8);
@@ -65,6 +79,8 @@ int *createBitImage(int height, int width, char *image, int imageSize)
 	return bitImage;
 }
 
+//Take the array of bits created and modify it accordinly to be 
+//ready to be insert in image
 int *createFinalBitImage(int height, int width, char *imageData, int biImageSize)
 {
 	int h = height, w = width;
@@ -98,6 +114,8 @@ int *createFinalBitImage(int height, int width, char *imageData, int biImageSize
 	return bitImage;
 }
 
+//Reverse engineer the above functions in order to get original bit array
+//To swap back andn create array of characters(text).
 void reverseFinalBitImage(int height, int width, int *bitImage)
 {
 	int h = height, w = width;
